51281c9bb560a3739d8d2f26daafc10f56914312
Allanzxz <allanevangelistabotelho@gmail.com>
2024-05-06 10:50:44 -0400

    findindex

---
+++ array/reduce.js
@@ -0,0 +1,5 @@
+let array = [1, 2, 3, 4, 5];
+let meuArray = array.reduce(function(acumulator, currentValue){
+     return acumulator + currentValue;
+}, 0);
+console.log(sun); // Saída (soma de todos os elementos)
