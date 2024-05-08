978e49f19f159bc1d3fbee936e4c30f21eb66eea
Allanzxz <allanevangelistabotelho@gmail.com>
2024-05-08 08:21:13 -0400

    exemplo de metodo keys

---
+++ objetos/object-values.js
@@ -0,0 +1,7 @@
+const person = {
+    name: "Allan",
+    age: "16",
+    city: "Cuiabá",
+};
+const values = Object.values(person);
+console.log(values);
---
+++ objetos/object.js
@@ -0,0 +1,7 @@
+const person = {
+    name: "Allan",
+    age: "16",
+    city: "Cuiabá",
+};
+const keys = Object.keys(person);
+console.log(keys); // Saída ["name" "age" "city"] 
